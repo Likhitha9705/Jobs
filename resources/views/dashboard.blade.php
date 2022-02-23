1c9a9543bdafe266c6502f8b50803ffa45928d0f
prashanth <87590885+Prashanthgoud7675@users.noreply.github.com>
2022-02-23 12:48:11 +0530

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,6 +7,7 @@
       <div class="flex justify-center">
           <div class="w-8/12 bg-white p-6 m-5 rounded-lg">
               home
+              <h1>iuewwksd</h1>
           </div>
       </div>
 
